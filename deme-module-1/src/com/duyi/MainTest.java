f54fee183254c9b9dd5ae4da109665a1eb3a5c47
SwordHeart123 <204866560@qq.com>
2023-02-18 15:08:23 +0800

    杀你妹妹

--- deme-module-1/src/com/duyi/MainTest.java
+++ deme-module-1/src/com/duyi/MainTest.java
@@ -2,10 +2,6 @@ package com.duyi;
 
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println("杀杀杀");
-
-        System.out.println("我我我");
-
-        System.out.println("你你你");
+        System.out.println("杀你妹妹");
     }
 }
